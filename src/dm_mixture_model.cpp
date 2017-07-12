e717ac55244cdc301accd668a0abab0b7ea341de
Chase Geigle <sky@skystrife.com>
2017-07-12 17:01:54 -0500

    Add implementation of the sampler.

--- src/dm_mixture_model.cpp
+++ src/dm_mixture_model.cpp
@@ -13,6 +13,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <random>
 
 #include "actions.h"
 #include "cpptoml.h"
@@ -66,6 +67,31 @@ class dm_mixture_model
         initialize(training, std::forward<RandomNumberEngine>(rng));
     };
 
+    template <class RandomNumberEngine>
+    void run(const training_data_type& training, uint64_t num_iters,
+             RandomNumberEngine&& rng)
+    {
+        LOG(progress) << "> Iteration 0 log joint likelihood: "
+                      << log_joint_likelihood() << "\n"
+                      << ENDLG;
+
+        for (uint64_t iter = 1; iter <= num_iters; ++iter)
+        {
+            printing::progress progress{" > Iteration " + std::to_string(iter)
+                                            + ": ",
+                                        topic_assignments_.size()};
+            progress.print_endline(false);
+
+            perform_iteration(progress, training,
+                              std::forward<RandomNumberEngine>(rng));
+            progress.clear();
+            LOG(progress) << "> Iteration " << iter
+                          << " log joint likelihood: " << log_joint_likelihood()
+                          << "\n"
+                          << ENDLG;
+        }
+    }
+
   private:
     template <class RandomNumberEngine>
     void initialize(const training_data_type& training,
@@ -74,6 +100,7 @@ class dm_mixture_model
         printing::progress progress{" > Initialization: ",
                                     topic_assignments_.size()};
 
+        // proceed like a normal sampling pass, but without removing any counts
         uint64_t x = 0;
         for (network_id i{0}; i < training.size(); ++i)
         {
@@ -82,7 +109,41 @@ class dm_mixture_model
                 auto z = sample_topic(i, training[i][j],
                                       std::forward<RandomNumberEngine>(rng));
                 topic_assignments_[x] = z;
-                topic_proportions_[i].increment(z);
+                topic_proportions_[i].increment(z, 1.0);
+                for (const auto& pr : training[i][j])
+                {
+                    topics_[z].increment(pr.first, pr.second);
+                }
+                progress(++x);
+            }
+        }
+    }
+
+    template <class RandomNumberEngine>
+    void perform_iteration(printing::progress& progress,
+                           const training_data_type& training,
+                           RandomNumberEngine&& rng)
+    {
+        uint64_t x = 0;
+        for (network_id i{0}; i < training.size(); ++i)
+        {
+            for (doc_id j{0}; j < training[i].size(); ++j)
+            {
+                // remove counts
+                auto old_z = topic_assignments_[x];
+                topic_proportions_[i].decrement(old_z, 1.0);
+                for (const auto& pr : training[i][j])
+                {
+                    topics_[old_z].decrement(pr.first, pr.second);
+                }
+
+                // sample new topic
+                auto z = sample_topic(i, training[i][j],
+                                      std::forward<RandomNumberEngine>(rng));
+                topic_assignments_[x] = z;
+
+                // update counts
+                topic_proportions_[i].increment(z, 1.0);
                 for (const auto& pr : training[i][j])
                 {
                     topics_[z].increment(pr.first, pr.second);
@@ -136,14 +197,53 @@ class dm_mixture_model
             auto gumbel_noise = -fastapprox::fastlog(-fastapprox::fastlog(rnd));
 
             if (log_prob + gumbel_noise > max_value)
+            {
                 result = z;
+                max_value = log_prob + gumbel_noise;
+            }
         }
 
         return result;
     }
 
+    double log_joint_likelihood() const
+    {
+        // log p(w, z) = log p(w | z)p(z) = log p(w|z) + log p(z)
+        //
+        // both p(w|z) and p(z) are Dirichlet-multinomial distributions
+        auto log_likelihood = 0.0;
+
+        // log p(w|z)
+        for (const auto& topic : topics_)
+        {
+            log_likelihood += dm_log_likelihood(topic);
+        }
+
+        // log p(z)
+        for (const auto& theta : topic_proportions_)
+        {
+            log_likelihood += dm_log_likelihood(theta);
+        }
+
+        return log_likelihood;
+    }
+
+    template <class T>
+    double dm_log_likelihood(const stats::multinomial<T>& dist) const
+    {
+        auto log_likelihood = std::lgamma(dist.prior().pseudo_counts());
+        log_likelihood -= std::lgamma(dist.counts());
+
+        dist.each_seen_event([&](const T& val) {
+            log_likelihood += std::lgamma(dist.counts(val));
+            log_likelihood -= std::lgamma(dist.prior().pseudo_counts(val));
+        });
+
+        return log_likelihood;
+    }
+
     /// the topic assignment for each session
-    std::vector<uint8_t> topic_assignments_;
+    std::vector<topic_id> topic_assignments_;
 
     /**
      * The action distributions for each role. Doubles as storage for the
@@ -160,31 +260,86 @@ class dm_mixture_model
 
 int main(int argc, char** argv)
 {
-    if (argc != 3)
+    if (argc < 3)
     {
-        std::cerr << "Usage: " << argv[0] << " config.toml" << std::endl;
+        std::cerr << "Usage: " << argv[0]
+                  << " config.toml network1 [network2] [network3]..."
+                  << std::endl;
         return 1;
     }
 
-    if (!filesystem::file_exists(argv[1]))
-    {
-        std::cerr << argv[1] << " not found" << std::endl;
-        return 1;
-    }
+    using training_data_type = dm_mixture_model::training_data_type;
+    using dm_sequences_type = dm_mixture_model::sequences_type;
+    using dm_session_type = dm_mixture_model::session_type;
 
-    using namespace sequence;
-    using namespace hmm;
-    using action_sequence_type = std::vector<state_id>;
-    using sequence_type = std::vector<action_sequence_type>;
-    using training_data_type = std::vector<sequence_type>;
+    using network_session_type = std::vector<action_type>;
+    using network_sequence_type = std::vector<network_session_type>;
+    using network_sequences_type = std::vector<network_sequence_type>;
 
     logging::set_cerr_logging();
 
-#if 0
-    std::ifstream input{argv[1], std::ios::binary};
-    LOG(info) << "Reading training data..." << ENDLG;
+    // set up options for the model
+    auto config = cpptoml::parse_file(argv[1]);
+    auto mix_config = config->get_table("dm-mixture-model");
+    if (!mix_config)
+    {
+        LOG(fatal) << "Missing dm-mixture-model table in " << argv[1] << ENDLG;
+        return 1;
+    }
+
+    dm_mixture_model::options_type options;
+
+    options.num_topics
+        = mix_config->get_as<uint8_t>("topics").value_or(options.num_topics);
+    options.alpha = mix_config->get_as<double>("alpha").value_or(options.alpha);
+    options.beta = mix_config->get_as<double>("beta").value_or(options.beta);
+
+    uint64_t total_sessions = 0;
     training_data_type training;
-    io::packed::read(input, training);
-#endif
+    for (int a = 2; a < argc; ++a)
+    {
+        if (!filesystem::file_exists(argv[a]))
+        {
+            LOG(fatal) << argv[a] << " not found!" << ENDLG;
+            return 1;
+        }
+
+        std::ifstream input{argv[a], std::ios::binary};
+
+        // need to convert ordered sequences -> histograms
+        // could write a separate extractor program for this, but why bother
+        network_sequences_type sequences;
+        auto bytes = io::packed::read(input, sequences);
+        if (bytes == 0 || !input)
+        {
+            LOG(fatal) << "Failed to read file " << argv[a] << ENDLG;
+            return 1;
+        }
+
+        dm_sequences_type network;
+        for (const auto& user : sequences)
+        {
+            for (const auto& session : user)
+            {
+                dm_session_type dm_session;
+                for (const auto& action : session)
+                    dm_session[action] += 1;
+                network.emplace_back(dm_session);
+            }
+        }
+        network.shrink_to_fit();
+
+        total_sessions += network.size();
+        training.emplace_back(std::move(network));
+    }
+
+    LOG(info) << "Read " << total_sessions << " sessions from "
+              << training.size() << " networks" << ENDLG;
+
+    std::mt19937 rng;
+    dm_mixture_model model{training, options, rng};
+    model.run(training, mix_config->get_as<uint64_t>("max-iter").value_or(1000),
+              rng);
+
     return 0;
 }
