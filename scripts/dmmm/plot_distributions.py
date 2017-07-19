5bf0ab9df99e1665e6888bb7cc32a8ab7339aa6f
Chase Geigle <sky@skystrife.com>
2017-07-19 18:25:07 -0500

    Adjust ylim in distribution plotting for DMMM.

--- scripts/dmmm/plot_distributions.py
+++ scripts/dmmm/plot_distributions.py
@@ -9,7 +9,7 @@ sns.set_style('whitegrid')
 
 for filename in os.listdir(sys.argv[1]):
     plt.figure()
-    plt.ylim(0, 0.65)
+    plt.ylim(0, 0.7)
     dset = pd.read_csv(os.path.join(sys.argv[1], filename))
     x = 'topic' if filename.endswith('proportions.csv') else 'action'
     plot = sns.barplot(x=x, y="probability", data=dset)
